d85b297675b09571d7744a6936d78e752757a7a1
frankSv <portillop2013@gmail.com>
2021-11-04 18:04:21 -0600

    Add initial data Script

---
+++ initial_data.sql
@@ -0,0 +1,25 @@
+insert into appointment_type values(1,'Personal');
+insert into appointment_type values(2,'Work');
+
+insert into info_type values (1,'Personal');
+insert into info_type values (2,'Work');
+
+insert into contact_type values(1,'Family');
+insert into contact_type values(2, 'Freinds');
+insert into contact_type values(3, 'Work');
+
+insert into contact values (1, 'San Salvador', 'Messi', 1);
+insert into contact values (2, 'San Miguel', 'Zidane', 2);
+insert into contact values (3, 'Santa Ana', 'Beto', 3);
+
+insert into phone values(1, '1234', 1, 1);
+insert into phone values(2, '5678', 2, 2);
+insert into phone values(3, '9876', 3, 1);
+
+insert into email values(1, 'abc@gmail.com', 1, 1);
+insert into email values(2, 'def@gmail.com', 2, 2);
+insert into email values(3, 'ghi@gmail.com', 3, 1);
+
+insert into appointment values(1, '20/11/2021', 'Family Party', 1);
+insert into appointment values(2, '20/11/2021', 'Job Interview', 2);
+insert into appointment values(3, '20/11/2021', 'Medical Appointment', 1);
